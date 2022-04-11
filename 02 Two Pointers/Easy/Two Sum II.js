a8db86eae472cdca794fa28a725ef2164be53054
Sunny Mei <sunmeiappprep@gmail.com>
2022-04-10 20:18:08 -0400

    moreq

--- 02 Two Pointers/Easy/Two Sum II.js
+++ 02 Two Pointers/Easy/Two Sum II.js
@@ -40,9 +40,25 @@
 
 
 var twoSum = function(numbers, target) {
-    
+    let left = 0
+    let right = numbers.length-1
+
+    while( right > left){
+        let sum =  numbers[left] + numbers[right]
+        if (sum === target){
+            return [left+1,right+1]
+        }
+        else if (sum > target){
+            right--
+        }
+        else{
+            left++
+        }
+    }
+
 };
 
-console.log()
-console.log()
-console.log()
+console.log(twoSum([2,7,11,15],9))
+console.log(twoSum([2,3,4],6))
+console.log(twoSum([-1,0],-1))
+
