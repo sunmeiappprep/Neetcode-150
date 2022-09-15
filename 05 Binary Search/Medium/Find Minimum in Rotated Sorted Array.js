693dccac66acefef1c49658314e766755c37bf45
Sunny Mei <sunmeiappprep@gmail.com>
2022-09-14 22:32:29 -0400

    BS median

--- 05 Binary Search/Medium/Find Minimum in Rotated Sorted Array.js
+++ 05 Binary Search/Medium/Find Minimum in Rotated Sorted Array.js
@@ -1,3 +1,32 @@
+var search = function(nums, target) {
+    let l = 0
+    let r = nums.length-1
+    let mid
+    while (r >= l){
+        console.log(nums.slice(l,r+1))
+        mid = Math.floor((l+r)/2)
+        if (nums[mid] === target){
+            return mid
+        }
+        if (nums[l] <= nums[mid]){
+            if (nums[l] > target || nums[mid] < target){
+                l = mid + 1
+            }
+            else{
+                r = mid -1
+            }
+        }else{
+            if (target > nums[r] || target < nums[mid]){
+                r = mid - 1
+            }
+            else {
+                l = mid + 1
+            }
+        }
+    }
+    return -1
+};
+
 console.log()
 console.log()
 console.log()
