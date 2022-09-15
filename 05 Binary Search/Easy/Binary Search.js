0018a322c7857f9827e9d059bfb03b7774f624ab
Sunny Mei <sunmeiappprep@gmail.com>
2022-09-14 22:24:54 -0400

    BS

--- 05 Binary Search/Easy/Binary Search.js
+++ 05 Binary Search/Easy/Binary Search.js
@@ -1,5 +1,22 @@
 var search = function(nums, target) {
+    let left = 0
+    let right = nums.length-1
+    let mid
     
+    while (right >= left){
+        mid = Math.floor((right+left)/2)
+        if (nums[mid] === target){
+            return mid
+        }
+        else if (nums[mid] < target){
+            left = mid + 1
+        }
+        else{
+            right = mid - 1
+        }
+    }
+    
+    return -1
 };
 
 console.log([-1,0,3,5,9,12],2)
