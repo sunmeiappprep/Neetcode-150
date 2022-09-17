0c566c41de6c126112ef8460413390d101f79873
Sunny Mei <sunmeiappprep@gmail.com>
2022-09-16 22:29:13 -0400

    LSSWi

--- 03 Sliding Window/Medium/Longest Substring Without Repeating Characters.js
+++ 03 Sliding Window/Medium/Longest Substring Without Repeating Characters.js
@@ -1,3 +1,19 @@
+var lengthOfLongestSubstring = function(s) {
+    let lastIndexFoundHash = {}
+    let l = 0
+    let longest = 0
+    for (let i = 0; i < s.length; i++) {
+        const char = s[i];
+        if (lastIndexFoundHash[char] >= l){
+            l = lastIndexFoundHash[char]+1
+        }
+        longest = Math.max(longest,i-l+1)
+        lastIndexFoundHash[char] = i
+    }
+        // console.log(lastIndexFoundHash)
+    return longest
+};
+
 console.log()
 console.log()
 console.log()
