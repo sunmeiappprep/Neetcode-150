e812c764a9cd9485130b31dbc9dc203ef0b5334c
Sunny Mei <sunmeiappprep@gmail.com>
2022-09-16 22:25:35 -0400

    Reverse LL

--- 06 Linked List/Easy/Reverse Linked List.js
+++ 06 Linked List/Easy/Reverse Linked List.js
@@ -1,3 +1,11 @@
-console.log()
-console.log()
-console.log()
+var reverseList = function(head) {
+    let prev = null
+    
+    while(head){
+        let temp = head.next
+        head.next = prev
+        prev = head
+        head = temp
+    }
+    return prev
+ };
