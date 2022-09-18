4881d9dd0af9cb71c9b1b29802857f466a22b0e7
Sunny Mei <sunmeiappprep@gmail.com>
2022-09-17 20:31:26 -0400

    trap

---
+++ 01 Arrays & Hashing/Tough/Trapping Rain Water.js
@@ -0,0 +1,28 @@
+var trap = function(height) {
+    let l = 0
+    let r = height.length-1
+    let sum = 0
+    let maxLeft = 0
+    let maxRight = 0
+    while (l<=r){
+        if (height[l] < height[r]){
+            if (height[l] > maxLeft) {
+                maxLeft = height[l]
+            }
+            else if( height[l] < maxLeft){
+                sum += maxLeft - height[l]
+            }
+            l++
+        }
+        else if (height[l] >= height[r]){
+            if (height[r] >= maxRight) {
+                maxRight = height[r]
+            }
+            else if( height[r] < maxRight){
+                sum += maxRight - height[r]
+            }
+            r--
+        }
+    }
+    return sum
+};
