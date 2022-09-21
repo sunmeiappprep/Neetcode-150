2cdd08b38ac360973b082245110141e7653324a8
Sunny Mei <sunmeiappprep@gmail.com>
2022-09-21 11:45:13 -0400

    still not sure where the 1 comes from

--- 07 Trees/Easy/Subtree of Another Tree.js
+++ 07 Trees/Easy/Subtree of Another Tree.js
@@ -1,3 +1,18 @@
-console.log()
-console.log()
-console.log()
+var diameterOfBinaryTree = function(root) {
+    let diameter = 0;
+    
+    dfs(root);
+    
+    return diameter;
+    
+    function dfs(node) {
+        if (!node) return 0;
+        
+        const left = dfs(node.left);
+        const right = dfs(node.right);
+        
+        diameter = Math.max(diameter, left + right);
+
+        return 1 + Math.max(left, right);
+    }
+};
