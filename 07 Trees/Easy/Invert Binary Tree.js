022cc917164d0a88533a6a5bc2b1ff55721a04ba
Sunny Mei <sunmeiappprep@gmail.com>
2022-09-23 09:35:52 -0400

    tree

--- 07 Trees/Easy/Invert Binary Tree.js
+++ 07 Trees/Easy/Invert Binary Tree.js
@@ -1,3 +1,9 @@
-console.log()
-console.log()
-console.log()
+var invertTree = function(root) {
+    if (root == null) return null;
+   
+    const r = invertTree(root.right);
+    const l = invertTree(root.left);
+    root.left = r;
+    root.right = l;
+    return root;
+};
