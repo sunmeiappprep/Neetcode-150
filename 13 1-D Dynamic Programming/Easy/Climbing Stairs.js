b22aaf745f34dbc99cb41cdcae3499826706bc39
Sunny Mei <sunmeiappprep@gmail.com>
2022-09-25 22:12:01 -0400

    cs

--- 13 1-D Dynamic Programming/Easy/Climbing Stairs.js
+++ 13 1-D Dynamic Programming/Easy/Climbing Stairs.js
@@ -1,3 +1,10 @@
-console.log()
-console.log()
-console.log()
+var climbStairs = function(n) {
+    if (n < 3) return n
+    let arr = [2,3]
+    let left = n-3
+    while ( left > 0){
+        arr.push(arr[arr.length-1]+arr[arr.length-2])
+        left--
+    }
+    return arr[arr.length-1]
+};
